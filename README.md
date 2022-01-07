6d59021c4c09800c58bd3efca39b17a735603d7d
Petr Styblo <styblope@gmail.com>
2022-01-07 12:39:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# caps-esc
+
+Tiny background utility for mapping <kbd>Esc</kbd> key to <kbd>CapsLock</kbd> key. Just like [xcape](https://github.com/alols/xcape) but for X11 and Wayland.
+
+## Usage
+```
+caps-esc /dev/input/event<x>
+```
+where `x` corresponds to the keyboard device event input.
+
+## Building and installation
+```
+sudo dnf install -y libevdev-devel
+cd caps-esc
+zig build -Drelease
+sudo install -s ./zig-out/bin/caps-esc /usr/local/bin
+```
+
+Create systemd unit file `/etc/systemd/system/caps-esc@.service`
+```
+[Unit]
+Description=Remap Esc key to CapsLock
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/caps-esc /dev/input/%i
+```
+
+Create udev rules `/etc/udev/rules.d/99-usb-keyboard.rules`
+```
+ACTION=="add", KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_INPUT_KEYBOARD}=="1", \
+ENV{DEVPATH}!="/devices/virtual/input/*", OWNER="root", TAG+="systemd", ENV{SYSTEMD_WANTS}="caps-esc@$kernel.service"
+```
+
+Initialize and run
+```
+$ systemctl daemon-reload 
+$ sudo udevadm control --reload
+```
+
+Check the status
+```
+$ systemctl status caps-esc@\*.service
+```
+
+Getting some device information
+```
+lsusb
+udevadm info -a -n /dev/input/event<x>
+```
+
+## References
+- http://blog.fraggod.net/2012/06/16/proper-ish-way-to-start-long-running-systemd-service-on-udev-event-device-hotplug.html
+- https://www.freedesktop.org/software/systemd/man/udev.html
+- https://www.freedesktop.org/software/libevdev/doc/latest/
+- https://ziglang.org/
+
+## Credits
+Inspired by [osm](https://github.com/ursm/osm) and [xcape](https://github.com/alols/xcape) 
---
+++ src/main.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("libevdev/libevdev.h");
+    @cInclude("libevdev/libevdev-uinput.h");
+});
+
+
+const FROM_KEY = c.KEY_CAPSLOCK;
+const TO_KEY   = c.KEY_ESC;
+
+pub fn main() anyerror!void {
+    var input_device: []u8 = undefined;
+    if (std.os.argv.len == 1) {
+        std.debug.print("Usage: {s} /dev/input/event<x>\n", .{std.os.argv[0]});
+        return error.ArgumentError;
+    }
+    input_device = std.mem.span(std.os.argv[1]);
+
+    const fd = try std.os.open(input_device, std.os.linux.O.RDWR, std.os.linux.S.IRWXU | std.os.linux.S.IRWXG | std.os.linux.S.IRWXO);
+    defer std.os.close(fd);
+
+    var dev: ?*c.libevdev = undefined;
+    if (c.libevdev_new_from_fd(fd, &dev) != 0) return error.EvdevError;
+    defer c.libevdev_free(dev);
+
+    if (c.libevdev_grab(dev, c.LIBEVDEV_GRAB) != 0) return error.EvdevGrabError;
+
+    std.log.info("Re-mapping keys on {s}, {s}", .{input_device, c.libevdev_get_name(dev)});
+
+    var uidev: ?*c.libevdev_uinput = undefined;
+    if (c.libevdev_uinput_create_from_device(dev, c.LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev) != 0) return error.UInputError;
+    defer c.libevdev_uinput_destroy(uidev);
+
+    std.log.info("Sending via {s}", .{c.libevdev_uinput_get_syspath(uidev)});
+
+    var hold: bool = false;
+    var ie: c.input_event = undefined;
+    while (true) {
+        if (c.libevdev_next_event(dev, c.LIBEVDEV_READ_FLAG_NORMAL, &ie) != 0) continue;
+        // re-map keys and write event to uinput
+        if (ie.type == c.EV_KEY) {
+            if (ie.code == FROM_KEY) {
+                switch (ie.value) {
+                    1 => hold = true,
+                    0 => if (hold) {
+                        // release original key
+                        try write_event(uidev, ie.type, ie.code, 0);
+                        // re-mapped key down
+                        try write_event(uidev, ie.type, TO_KEY, 1);
+                        // re-mapped key up
+                        try write_event(uidev, ie.type, TO_KEY, 0);
+                    },
+                    else => hold = false
+                }
+            } else hold = false;
+        }
+        if (c.libevdev_uinput_write_event(uidev, ie.type, ie.code, ie.value) != 0) return error.UInputEventWriteError;
+    }
+}
+
+fn write_event(uinput_dev: ?*c.libevdev_uinput, ev_type: c_uint, ev_code: c_uint, ev_value: c_int) anyerror!void {
+    if (c.libevdev_uinput_write_event(uinput_dev, ev_type, ev_code, ev_value) != 0) return error.UInputEventWriteError;
+    if (c.libevdev_uinput_write_event(uinput_dev, c.EV_SYN, c.SYN_REPORT, 0) != 0) return error.UInputEventWriteError;
+}
